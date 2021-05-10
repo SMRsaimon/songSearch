9590119fd926789176803b495652fa2f317665a2
SMRsaimon <smrsaimon960@gmail.com>
2021-05-10 11:26:29 -0700

    complete without redux

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,38 +1,77 @@
 import React, { useEffect, useState } from "react";
-import "./Home.css"
-import { Card, Container, Text } from "./style";
+import "./Home.css";
+import { Card, CardContainer, Container, Text } from "./style";
 import { AiOutlineSearch } from "react-icons/ai";
 import RepositoryList from "../RepositoryList";
-
+import SearchResult from "../SearchResult";
 
 const Home = () => {
-  const [searchResult, setSearchResult] = useState([]);
+  const [result, setResult] = useState([]);
+  const [searchResult, setSearchResult] = useState("");
 
   useEffect(() => {
-    fetch(`https://itunes.apple.com/search?term=a`)
+    fetch(`https://itunes.apple.com/search?term=${searchResult}`)
       .then((res) => res.json())
       .then((data) => {
-        setSearchResult(data.results);
+        setResult(data.results);
       });
-  }, []);
+  }, [searchResult, setSearchResult]);
+  const hendelSearch = (e) => {
+   
+    if(e.target.value){
+        setSearchResult(e.target.value);
+    }else{
 
-  console.log(searchResult);
+        setResult([]);
+    }
+  
+    
+  };
+  console.log(searchResult)
   return (
     <Container>
+      <Card>Repository Search</Card>
       <Card>
-      Repository Search
-      </Card>
-      <Card>
-         <Text>Get details of repositories</Text>
-          <span className="searchContainer">
-          <input className="search-box" type="search" name="search" id="search" />
-            <span>
-                <AiOutlineSearch/>
-            </span>
+        <Text>Get details of repositories</Text>
+        <span className="searchContainer">
+          <input
+            className="search-box"
+            type="search"
+            onChange={(e) => hendelSearch(e)}
+          
+            name="search"
+            id="search"
+          />
+          <span>
+            <AiOutlineSearch />
           </span>
+        </span>
       </Card>
-      <RepositoryList/>
-    </Container>
+
+      {
+      
+      
+      result.length===0?<><RepositoryList /> </>
+      
+      :<>
+      
+      <CardContainer style={{ marginTop: "50px" }}>
+        <Text>Search query: {searchResult}</Text>
+        <Text>Total number of matching repos: {result.length}</Text>
+
+        {result.map((x) => (
+          <SearchResult result={x} />
+        ))}
+      </CardContainer>
+      
+      </>
+
+      }
+
+      </Container>
+      
+
+  
   );
 };
 
---
+++ src/components/SearchResult.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Card, Text } from "./Home/style";
+
+const SearchResult = ({searchResult, result}) => {
+ const {artistName,collectionName,collectionPrice}=result
+    
+  return (
+    <>
+        
+        <Card style={{ width: "90%" }}>
+        <Text>Artist Name: {artistName}</Text>
+        <Text>Collection Name: {collectionName}</Text>
+        <Text>Collection Price: {collectionPrice}</Text>
+        </Card>
+    
+    </>
+  );
+};
+
+export default SearchResult;
